7292f2d970cdc4c3f1b9e3ba1ff1c45a23c17cb2
GaLaiLapTrinh <galailaptrinh@gmail.com>
2025-05-25 16:36:18 +0700

    update up to 18.1 refs and portals

---
+++ 18.0-refs - portals -startproject/src/App.jsx
@@ -0,0 +1,12 @@
+import Player from "./components/Player.jsx";
+import Header from "./components/Header.jsx";
+
+function App() {
+  return (
+    <>
+      <div id="challenges"></div>
+    </>
+  );
+}
+
+export default App;
